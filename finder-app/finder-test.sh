85033d88b008399cd3ea7f1c1e15d3489f5caf78
HemQL <heli1944@colorado.edu>
2025-04-29 09:59:40 -0400

    fix writer and finder.sh location for path support

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,15 +49,15 @@ then
 		exit 1
 	fi
 fi
-echo "Removing the old writer utility and compiling as a native application"
+
 
 
 for i in $( seq 1 $NUMFILES)
 do
-	./${PATH}/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./${PATH}/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
