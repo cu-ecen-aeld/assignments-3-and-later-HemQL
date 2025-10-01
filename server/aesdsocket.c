966891c58ab7722bf0d530c0684218390a4c8f57
HemQL <heli1944@colorado.edu>
2025-10-01 14:06:24 -0400

    First draft

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,12 +10,34 @@
 #include <syslog.h>
 #include <signal.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/queue.h>
+#include <time.h>
 #define CHUNK_SIZE 256
 
-int sfd, cfd;
+int sfd, client_fd;
 int fd;
 char* buf;
 char* rdfile;
+int bufsize = 256;
+int rdsize = 256;
+int status;
+int errsv;
+pthread_t timestamps;
+time_t lastStamp;
+pthread_mutex_t fd_mut = PTHREAD_MUTEX_INITIALIZER;
+
+// Define a structure for the list entries
+struct thread_entry {
+    pthread_t thread;
+    int id;
+    //int cfd;
+    int complete;
+    SLIST_ENTRY(thread_entry) entries; // Singly-linked list pointer
+};
+
+// Declare the head of the list
+SLIST_HEAD(slisthead, thread_entry) head;
 
 
 
@@ -28,13 +50,23 @@ void cleanup(int exit_code) {
     }
     free(buf);
     free(rdfile);
-    if (cfd != -1) {
-        shutdown(cfd, SHUT_RDWR);
-    }
+//    if (cfd != -1) {
+//        shutdown(cfd, SHUT_RDWR);
+//    }
     if (sfd != -1) {
         shutdown(sfd, SHUT_RDWR);
+        close(sfd);
     }
 
+    struct thread_entry *n1;
+    while (!SLIST_EMPTY(&head)) {           /* List deletion */
+        n1 = SLIST_FIRST(&head);
+        SLIST_REMOVE_HEAD(&head, entries);
+        free(n1);
+
+
+    }
+    pthread_mutex_destroy(&fd_mut);
     exit(exit_code);
     
 }
@@ -47,18 +79,133 @@ void handle_signal(int sig, siginfo_t *info, void *context) {
     cleanup(errsv == 0);
 }
 
+static void *writeTimestamp(){
+    int outstr_size;
+    char outstr[33];
+    time_t t;
+    
+    struct tm *tmp;
+    //printf("%s", outstr);
+    while (1){
+        t = time(NULL);
+        tmp = localtime(&t);
+        if((long)t-(long)lastStamp>10){
+            if (strftime(outstr, sizeof(outstr), "%a, %d %b %Y %T %z\n", tmp) == 0) {
+                syslog(LOG_DEBUG, "strftime returned 0");
+            }
+            else{
+                syslog(LOG_DEBUG, "Writing timestamp to file: %s", outstr);
+                pthread_mutex_lock(&fd_mut);
+                outstr_size = write(fd, outstr, sizeof(outstr));
+                pthread_mutex_unlock(&fd_mut);
+                lastStamp = t;
+                if (outstr_size != 33) {
+                    syslog(LOG_DEBUG, "strftime length: %i", outstr_size);
+                }
+            }
+
+    	}
+        else{
+        	syslog(LOG_DEBUG, "Too soon for timestamp.");
+        	sleep(1);
+        } 
+        
+    }
+    pthread_exit(0);
+}
+
+static void *writeToFile(void * entry){
+
+    
+    ssize_t nr;
+        
+    int errsv;
+    int rcvd;
+
+    struct thread_entry *thread_info = (struct thread_entry *) entry;
+    //int cfd = thread_info->cfd;
+    int cfd = client_fd;
+    pthread_mutex_lock(&fd_mut);
+    rcvd = recv(cfd, buf, bufsize, 0);
+    while (rcvd == bufsize) {
+        buf = realloc(buf, bufsize + CHUNK_SIZE);
+        if (!buf) {
+            syslog(LOG_ERR,"Unable to realloc CHUNK_SIZE more to %i", bufsize);
+            cleanup(1);
+        }
+        ssize_t rcvd_more = recv(cfd, buf+bufsize, CHUNK_SIZE, 0);
+        rcvd = rcvd + rcvd_more;
+        bufsize = bufsize+CHUNK_SIZE;
+    } 
+    
+    nr = write(fd, (char *)buf, rcvd);
+
+    //printf("Writing %s to file\n", buf);
+  
+    syslog(LOG_DEBUG, "Writing %i bytes to file", rcvd);
+    if (nr == -1){
+        errsv = errno;
+        syslog(LOG_ERR,"Error writing data, %s", strerror(errsv));
+        cleanup(-1);
+    }
+    else if (nr != rcvd){
+        syslog(LOG_ERR,"Write size does not match size of string to write");
+    }
+        
+    int filesize = (int)lseek(fd,0,SEEK_END); 
+    lseek(fd,0,SEEK_SET);    
+        
+    if (filesize>rdsize){
+        free(rdfile);
+        rdfile = malloc(filesize);
+        if (rdfile == NULL){
+            syslog(LOG_ERR,"malloc failed to allocate %i bytes.",filesize);
+            cleanup(1);
+            
+        }
+    }
+    
+
+    if (read(fd, rdfile, filesize) == -1){
+        errsv = errno;
+        syslog(LOG_ERR,"Error reading file, %s\n", strerror(errsv));
+        cleanup(-1);
+    }
+    pthread_mutex_unlock(&fd_mut);
+    int sent = send(cfd, rdfile, filesize, 0);
+            
+    if (sent == -1){
+        errsv = errno;
+        syslog(LOG_ERR,"Error sending back file contents, %s\n", strerror(errsv));
+        syslog(LOG_DEBUG,"cfd = %i\n", cfd);
+        cleanup(-1);
+    }
+    
+    syslog(LOG_USER,"Sent %i bytes from file", sent);
+    pthread_mutex_unlock(&fd_mut);
+    usleep(filesize*100);
+    if (close(cfd) == -1){
+        errsv = errno;
+        syslog(LOG_ERR,"Thread %i: close error: %s", thread_info->id, strerror(errsv));
+        cleanup(-1);
+    }
+    thread_info->complete = 1;
+    pthread_exit(0);    
+}
 
 int main(int argc, char **args){
-
+    lastStamp = time(NULL);
+    int count = 1;
+    SLIST_INIT(&head);
+    
     struct sigaction act = { 0 };
     act.sa_sigaction = &handle_signal;
 
-    int errsv;
-    int rcvd;
-    int bufsize = 256;
-    int rdsize = 256;
     buf = malloc(bufsize);
     rdfile = malloc(rdsize);
+        
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
     
     syslog(LOG_USER, "Initializing aesdsocket server...");
     int i;
@@ -67,28 +214,20 @@ int main(int argc, char **args){
             fprintf(stdout, "%s ", (char *)args[i]);
         }
         int pid = fork();
- 
-        if (pid !=0){
+        if (pid == -1){
+            syslog(LOG_USER, "aesdsocket daemon failed to start.");
+            return 0;
+        }
+        else if (pid !=0){
             syslog(LOG_USER, "aesdsocket daemon started, PID: %u", pid);
             return 0;
         }
         
     }
     
+    //int root_pid = getpid();
     openlog(NULL,0,LOG_USER);
     
-    fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT, 0666);
-    if (fd == -1){
-	errsv = errno;
-//        fprintf(stderr,"Error creating file, %s\n", strerror(errsv));
-        syslog(LOG_ERR,"Error creating file, %s\n", strerror(errsv));
-        return(-1);
-    }
-    int status;
-    
-    ssize_t nr;
-    struct addrinfo hints;
-    struct addrinfo *servinfo;
     
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -124,104 +263,67 @@ int main(int argc, char **args){
     struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&c_addr;
     socklen_t clen = servinfo->ai_addrlen;
     freeaddrinfo(servinfo);  
+
+    fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT, 0666);
+
     
-    while(1){        
+    if (fd == -1){
+	errsv = errno;
+        syslog(LOG_ERR,"Error creating file, %s\n", strerror(errsv));
+        return(-1);
+    }
+    
+    pthread_create(&timestamps, NULL, writeTimestamp, NULL);
+    
+    
+    while(1){
         if (sigaction(SIGTERM, &act, NULL) == -1) {
-               perror("sigaction");
-               exit(EXIT_FAILURE);
+            perror("sigaction");
+            exit(EXIT_FAILURE);
         }
         if (sigaction(SIGINT, &act, NULL) == -1) {
-               perror("sigaction");
-               exit(EXIT_FAILURE);
+            perror("sigaction");
+            exit(EXIT_FAILURE);
         }
-        
-        cfd = accept(sfd, c_addr, &clen);
+        int cfd = accept(sfd, c_addr, &clen);
         if (cfd == -1) {
             errsv = errno;
             syslog(LOG_ERR,"accept error: %s\n", strerror(errsv));
             cleanup(-1);
         }
-        
-        
-        
-    
-        /* Code to deal with incoming connection(s)... */
-        
+        //writeTimestamp();  
         struct in_addr ipAddr = pV4Addr->sin_addr;
-
         char str[INET_ADDRSTRLEN];
         inet_ntop( AF_INET, &ipAddr, str, INET_ADDRSTRLEN );
         syslog(LOG_USER,"Accepted connection from %s", str);
-    
-        rcvd = recv(cfd, buf, bufsize, 0);
-        while (rcvd == bufsize) {
-            buf = realloc(buf, bufsize + CHUNK_SIZE);
-            if (!buf) {
-                syslog(LOG_ERR,"Unable to realloc CHUNK_SIZE more to %i", bufsize);
-                cleanup(1);
+        
+        struct thread_entry *node1 = malloc(sizeof(struct thread_entry));
+
+        SLIST_INSERT_HEAD(&head, node1, entries); // Insert at the head
+        node1->id = count;
+        count++;  
+        node1->complete = 0;
+        //node1->cfd = cfd;
+        client_fd = cfd;     
+        //writeToFile((void*)node1);
+        
+        pthread_create(&node1->thread, NULL, writeToFile, &node1);
+        struct thread_entry *current;
+        
+        SLIST_FOREACH(current, &head, entries){
+            if (current->complete == 1){
+                //syslog(LOG_USER, "%i: write complete", current->id);
+                printf("%i: write complete, removing entry\n", current->id);
+                pthread_join(current->thread, NULL);
+                SLIST_REMOVE(&head, current, thread_entry, entries);
             }
-            ssize_t rcvd_more = recv(cfd, buf+bufsize, CHUNK_SIZE, 0);
-            rcvd = rcvd + rcvd_more;
-            bufsize = bufsize+CHUNK_SIZE;
+   
         }
-        
-
-        
-        nr = write(fd, (char *)buf, rcvd);
-  
-        syslog(LOG_DEBUG, "Writing %s to file, total %i bytes", (char *)buf, rcvd);
-        if (nr == -1){
-    /* error, check errno */
-    	errsv = errno;
-    //fprintf(stderr,"Error writing data, %s\n", strerror(errsv));
-            syslog(LOG_ERR,"Error writing data, %s", strerror(errsv));
-            cleanup(-1);
-        }
-        else if (nr != rcvd){
-    //fprintf(stderr,"Write size does not match size of string to write");
-            syslog(LOG_ERR,"Write size does not match size of string to write");
-    
-        }
-        
-        int filesize = (int)lseek(fd,0,SEEK_END); 
-        lseek(fd,0,SEEK_SET);    
-        
-        if (filesize>rdsize){
-            free(rdfile);
-            rdfile = malloc(filesize);
-            if (rdfile == NULL){
-                syslog(LOG_ERR,"malloc failed to allocate %i bytes.",filesize);
-                cleanup(1);
-                
-            }
-        }
-
-        if (read(fd, rdfile, filesize) == -1){
-            errsv = errno;
-            syslog(LOG_ERR,"Error reading file, %s\n", strerror(errsv));
-            cleanup(-1);
-        }
-        int sent = send(cfd, rdfile, filesize, 0);
-            
-        if (sent == -1){
-            errsv = errno;
-            syslog(LOG_ERR,"Error sending data, %s\n", strerror(errsv));
-            cleanup(-1);
-        }
-        syslog(LOG_USER,"Sent %i bytes from file", sent);
-        usleep(filesize*100);
-    
-
-        if (close(cfd) == -1){
-            syslog(LOG_ERR,"close error: %s", strerror(status));
-            cleanup(-1);
-        }
-        
     }
-    
     cleanup(1);
 
 }
+
 
 
     
